4f5eee1dad9e1d1cfb59aa5a1c1a60a779f9d025
Boyko Amarov <boyko.amarov@gmail.com>
2018-04-26 20:06:47 +0300

    Initial upload

---
+++ Problem_1.R
@@ -0,0 +1,21 @@
+
+# Homework 3
+# Problem 1
+
+# Install the following packages if necessary. Uncomment the following line and execute it.
+# install.packages(c('dplyr', 'ggplot2'))
+
+# Load the packages
+
+library(ggplot2)
+
+# Download and read the data
+wines <- read.csv(file='https://s3.eu-central-1.amazonaws.com/sf-timeseries/data/wine.csv')
+
+## a)
+
+## b)
+
+## c)
+
+## d)
---
+++ Problem_2.R
@@ -0,0 +1,29 @@
+# Homework 2
+# Problem 2
+
+# Install the following packages if necessary
+# install.packages(c('ggplot2', 'dplyr'))
+
+library(dplyr)
+library(ggplot2)
+
+# Enter your student ID in set.seed() so that your results are reproducible
+
+# use the rnorm function to generate values from the standard normal distribution
+# you can compute e^x with the exp function, e.g. e^2 = exp(2).
+
+## a)
+
+## b)
+
+## c)
+
+## d)
+
+## e)
+
+## f)
+
+## g)
+
+## h)
